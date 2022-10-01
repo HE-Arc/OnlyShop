3bec3866ab9cf5df9b4e738719d654cbf650e285
Lucas Perrin <lucas.perrin1@he-arc.ch>
2022-10-01 14:32:23 +0200

    Init vue
    https://techvblogs.com/blog/how-to-install-vue-3-in-laravel-9-with-vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue";
+import App from "./app/App.vue";
+
+createApp(App).mount("#app");
---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>OnlyShop</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div id="app">
+        <!--
+            This is where the content will be rendered
+            -> by createApp(App).mount("#app"); in App.vue
+        -->
+    </div>
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
