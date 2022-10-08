4cb48d1f5b237b855ed8475d1d3e5705fb90aa0e
ruimarco <rui.loureiro@he-arc.ch>
2022-10-08 16:40:21 +0200

    Add vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 import "./bootstrap";
-
+import "vuetify/styles"; //Global CSS
 import { createApp } from "vue";
+import { createVuetify } from 'vuetify';
 import App from "./app/App.vue";
 
-createApp(App).mount("#app");
+
+const app = createApp(App);
+const vuetify = createVuetify();
+
+app.use(vuetify);
+app.mount("#app");
