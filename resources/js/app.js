1cc9a6582e6a6eeb1f1f75ce0fe270aced28ee16
ruimarco <rui.loureiro@he-arc.ch>
2022-10-11 10:41:43 +0200

    Solve icon problem (WIP) + add carousel for items

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,22 @@ import "./bootstrap";
 import "vuetify/styles"; //Global CSS
 import { createApp } from "vue";
 import { createVuetify } from "vuetify";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css';
 import App from "./app/App.vue";
 import pinia from "./app/store";
 
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import "material-design-icons-iconfont/dist/material-design-icons.css";
+
+
 
 const vuetify = createVuetify({
     components,
     directives,
 });
+
 
 const app = createApp(App);
 app.use(pinia);
--- resources/js/app/Items.vue
+++ resources/js/app/Items.vue
@@ -1,14 +1,15 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import { useStore as useItemsStore } from "./store/item.store";
 
-const show = ref(false)
+const show = ref(false);
 const itemsStore = useItemsStore();
 const { items, loading, error } = storeToRefs(itemsStore);
+const model = 0;
+const colors = ["primary", "secondary", "yellow", "red", "orange"];
 
 itemsStore.fetchItems();
-
 </script>
 
 <template>
@@ -21,44 +22,88 @@ itemsStore.fetchItems();
         <div v-if="items">
             <v-container class="bg-surface-variant">
                 <v-row no-gutters>
-                    <v-col v-for="item in items" :key="item.id" cols="12" sm="4">
+                    <v-col
+                        v-for="item in items"
+                        :key="item.id"
+                        cols="12"
+                        sm="4"
+                    >
                         <v-sheet class="ma-2 pa-2">
                             <v-card class="mx-auto" max-width="344">
-                                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover>
-                                </v-img>
+                                <v-carousel>
+                                    <v-carousel-item
+                                        v-for="color in colors"
+                                        :key="color"
+                                    >
+                                        <v-sheet
+                                            :color="color"
+                                            height="100%"
+                                            tile
+                                        >
+                                            <v-row
+                                                class="fill-height"
+                                                align="center"
+                                                justify="center"
+                                            >
+                                                <div class="text-h2">
+                                                    {{ color }}
+                                                </div>
+                                            </v-row>
+                                        </v-sheet>
+                                    </v-carousel-item>
+                                </v-carousel>
 
-                                <v-card-title> {{item.name}} </v-card-title>
+                                <v-card-title> {{ item.name }} </v-card-title>
 
-                                <v-card-subtitle> {{item.price}} </v-card-subtitle>
+                                <v-card-subtitle>
+                                    {{ item.price }}
+                                </v-card-subtitle>
 
                                 <v-card-action>
-                                    <v-btn><i class="material-icons"> payment </i></v-btn>
+                                    <v-btn
+                                        ><i class="material-icons">
+                                            payment
+                                        </i></v-btn
+                                    >
                                     <v-btn @click="show = !show">
                                         <i v-if="show" class="material-icons">
                                             close
                                         </i>
-                                        <i v-else class="material-icons"> add </i>
+                                        <i v-else class="material-icons">
+                                            add
+                                        </i>
                                     </v-btn>
 
-                                    <v-btn href="/item/1"><i class="material-icons"> edit </i></v-btn>
-                                    <v-btn @click="itemsStore.deleteItem(item.id)"><i class="material-icons"> delete
-                                        </i></v-btn>
+                                    <v-btn href="/item/1"
+                                        ><i class="material-icons">
+                                            edit
+                                        </i></v-btn
+                                    >
+                                    <v-btn
+                                        @click="itemsStore.deleteItem(item.id)"
+                                        ><i class="material-icons">
+                                            delete
+                                        </i></v-btn
+                                    >
                                 </v-card-action>
                                 <v-expand-transition>
                                     <div v-show="show">
                                         <v-divider></v-divider>
                                         <v-card-text>
-                                            Lorem ipsum dolor sit amet, consectetur
-                                            adipiscing elit, sed do eiusmod tempor
-                                            incididunt ut labore et dolore magna aliqua.
-                                            Ut enim ad minim veniam, quis nostrud
-                                            exercitation ullamco laboris nisi ut aliquip
-                                            ex ea commodo consequat. Duis aute irure
-                                            dolor in reprehenderit in voluptate velit
-                                            esse cillum dolore eu fugiat nulla pariatur.
-                                            Excepteur sint occaecat cupidatat non
-                                            proident, sunt in culpa qui officia deserunt
-                                            mollit anim id est laborum.
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore
+                                            et dolore magna aliqua. Ut enim ad
+                                            minim veniam, quis nostrud
+                                            exercitation ullamco laboris nisi ut
+                                            aliquip ex ea commodo consequat.
+                                            Duis aute irure dolor in
+                                            reprehenderit in voluptate velit
+                                            esse cillum dolore eu fugiat nulla
+                                            pariatur. Excepteur sint occaecat
+                                            cupidatat non proident, sunt in
+                                            culpa qui officia deserunt mollit
+                                            anim id est laborum.
                                         </v-card-text>
                                     </div>
                                 </v-expand-transition>
@@ -68,6 +113,5 @@ itemsStore.fetchItems();
                 </v-row>
             </v-container>
         </div>
-
     </div>
 </template>
