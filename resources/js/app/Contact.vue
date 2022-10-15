84e3f50f9cf2578c097c1f35c1055fa29e17b2ea
ruimarco <rui.loureiro@he-arc.ch>
2022-10-15 17:25:11 +0200

    Add contact page

--- resources/js/app/Contact.vue
+++ resources/js/app/Contact.vue
@@ -10,4 +10,30 @@ updated by : Rui Marco Loureiro
 
 <template>
     <h1>Contact</h1>
+    <v-card class="mx-auto" max-width="60%" max-height="90%" variant="outlined">
+        <v-card-item>
+            <div>
+                <div class="text-h6 mb-1">Contact us</div>
+
+                <div class="text-caption">
+                    <v-form lazy-validation>
+                        <v-text-field
+                            label="Email"
+                            name="email"
+                            type="email"
+                        ></v-text-field>
+
+                        <v-textarea
+                            label="Description"
+                            name="text"
+                        ></v-textarea>
+                    </v-form>
+                </div>
+            </div>
+        </v-card-item>
+
+        <v-card-actions>
+            <v-btn variant="outlined"> Validate </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
