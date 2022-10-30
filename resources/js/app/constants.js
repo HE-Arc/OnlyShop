f9ea44f8004a2b4def5c4bd99a33046fbcef8488
ruimarco <rui.loureiro@he-arc.ch>
2022-10-30 22:14:22 +0100

    wip

--- resources/js/app/constants.js
+++ resources/js/app/constants.js
@@ -1,12 +1,11 @@
-
-
 const env = import.meta.env.VITE_APP_ENV;
 
-const location = "http://localhost:8000/api";
+let location = "http://localhost:8000/api";
 
 if(env === 'production') {
     location = "https://onlyshop.k8s.ing.he-arc.ch/api";
 }
 
+console.log(location);
 export const API_LOCATION=location;
 
